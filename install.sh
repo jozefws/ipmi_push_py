cea4f04e2806dc7bc465a3593ecf1996885ae01b
Joe <joe@sieniawski.co.uk>
2023-02-23 16:04:53 +0000

    Created bash install script and removed influx_token from env example

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo python3 -m pip install -r requirements.txt
+sudo cp .env.example .env
+echo Enter influx write token:
+read token
+echo -e INFLUX_TOKEN=$token >> .env
+(sudo crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /home/roottsg/ipmi_push_py/py_ipmi_influx.py") | crontab -
