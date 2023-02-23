c545a733f566f9818a8b4be8b3a0ee5775b28829
Joe <joe@sieniawski.co.uk>
2023-02-23 16:36:19 +0000

    Updated script directory

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 sudo python3 -m pip install -r requirements.txt
 sudo cp .env.example .env
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo -e "\n\n\n"
 echo Enter influx write token:
 read token
 printf '\nINFLUX_TOKEN="$token"' >> .env
-(sudo crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 $pwd/py_ipmi_influx.py") | crontab -
+(sudo crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 $script_dir/py_ipmi_influx.py") | crontab -
 
 echo -e "\nInstallation complete. Please edit .env file to change server details if needed.\n"
