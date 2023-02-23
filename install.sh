4324375cdd974c5b995d52d25b31619b54313759
Joe <joe@sieniawski.co.uk>
2023-02-23 16:44:24 +0000

    Updated install escaped quotes for token

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo -e "\n\n\n"
 echo Enter influx write token:
 read token
-printf '\nINFLUX_TOKEN="$token"' >> .env
+printf "\nINFLUX_TOKEN=\"$token\"" >> .env
 (sudo crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 $script_dir/py_ipmi_influx.py") | crontab -
 
 echo -e "\nInstallation complete. Please edit .env file to change server details if needed.\n"
