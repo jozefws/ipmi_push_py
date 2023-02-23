14f5d92662098e154adc8d59738763f04caa77a2
Joe <joe@sieniawski.co.uk>
2023-02-23 16:21:49 +0000

    Updated install script to include spacing and working directory o f installation

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 sudo python3 -m pip install -r requirements.txt
 sudo cp .env.example .env
+echo -e "\n\n\n"
 echo Enter influx write token:
 read token
 printf '\nINFLUX_TOKEN="$token"' >> .env
-(sudo crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /home/roottsg/ipmi_push_py/py_ipmi_influx.py") | crontab -
+(sudo crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 $pwd/py_ipmi_influx.py") | crontab -
+
+echo -e "\nInstallation complete. Please edit .env file to change server details if needed.\n"
