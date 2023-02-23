24299e3ce2f26782fc88d5ee0729ea90bc3cf8a5
Joe <joe@sieniawski.co.uk>
2023-02-23 16:09:16 +0000

    Updated install script to have secret read and printf

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo python3 -m pip install -r requirements.txt
 sudo cp .env.example .env
 echo Enter influx write token:
-read token
-echo -e INFLUX_TOKEN=$token >> .env
+read -s token
+printf "\nINFLUX_TOKEN=$token" >> .env
 (sudo crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /home/roottsg/ipmi_push_py/py_ipmi_influx.py") | crontab -
